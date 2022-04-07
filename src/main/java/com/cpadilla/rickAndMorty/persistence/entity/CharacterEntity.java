653844cd0c0d0c1d3346686324dba9c32e6193d0
CristianPadilla <lozadacristian21@gmail.com>
2022-04-07 10:58:46 -0500

    first commit

---
+++ src/main/java/com/cpadilla/rickAndMorty/persistence/entity/CharacterEntity.java
@@ -0,0 +1,9 @@
+package com.cpadilla.rickAndMorty.persistence.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "")
+public class CharacterEntity {
+}
